3b46ca51876714c2da73143f8a6377d4ea9fd902
Ledespana <no-reply@ledespana.com>
2024-07-17 13:40:27 +0200

    d

--- contracts/BasicLSP8.sol
+++ contracts/BasicLSP8.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.12;
 
 // modules
-import {
-    LSP8Mintable
-} from "@lukso/lsp-smart-contracts/contracts/LSP8IdentifiableDigitalAsset/presets/LSP8Mintable.sol";
+import {LSP8Mintable} from "@lukso/lsp-smart-contracts/contracts/LSP8IdentifiableDigitalAsset/presets/LSP8Mintable.sol";
 
 contract BasicLSP8 is LSP8Mintable {
     constructor(
