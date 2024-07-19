3812baaf29381979707eb58283bbfb80b069bb70
Ledespana <no-reply@ledespana.com>
2024-07-19 14:58:13 +0200

    d

--- contracts/BasicLSP8CollectionOfCollections.sol
+++ contracts/BasicLSP8CollectionOfCollections.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.12;
 
 // modules
-import {BasicLSP8} from "./BasicLSP8.sol";
+import {LSP8SubCollection} from "./LSP8SubCollection.sol";
 import {LSP8IdentifiableDigitalAsset} from "@lukso/lsp-smart-contracts/contracts/LSP8IdentifiableDigitalAsset/LSP8IdentifiableDigitalAsset.sol";
 import {_LSP8_TOKENID_FORMAT_ADDRESS} from "@lukso/lsp-smart-contracts/contracts/LSP8IdentifiableDigitalAsset/LSP8Constants.sol";
 import {_LSP4_METADATA_KEY} from "@lukso/lsp-smart-contracts/contracts/LSP4DigitalAssetMetadata/LSP4Constants.sol";
@@ -31,15 +31,17 @@ contract BasicLSP8CollectionOfCollections is LSP8IdentifiableDigitalAsset {
         string memory symbolOfLSP8_,
         uint256 lsp4TokenType_,
         uint256 lsp8TokenIdFormat_,
-        address receiver_
+        address receiver_,
+        bytes memory lsp4MetadataURIOfLSP8_
     ) public onlyOwner returns (address lsp8SubCollectionAddress) {
         // deploy the BasicLSP8 contract and set the address as tokenId
-        BasicLSP8 lsp8SubCollection = new BasicLSP8(
+        LSP8SubCollection lsp8SubCollection = new LSP8SubCollection(
             nameOfLSP8_,
             symbolOfLSP8_,
             address(this), // owner of the LSP8SubCollection is this contract (LSP8Collection contract)
             lsp4TokenType_,
-            lsp8TokenIdFormat_
+            lsp8TokenIdFormat_,
+            lsp4MetadataURIOfLSP8_
         );
 
         lsp8SubCollectionAddress = address(lsp8SubCollection);
