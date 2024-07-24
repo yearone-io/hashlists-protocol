1ba62aece3f0ffd09aae1cc8dd6a6fe428f119e7
Ledespana <no-reply@ledespana.com>
2024-07-24 17:58:04 +0200

    d

---
+++ contracts/CuratedListLibrary.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+import {CuratedListCollection} from "./CuratedListCollection.sol";
+
+library CuratedListLibrary {
+    function deployCuratedList(
+        string memory curatedListName,
+        string memory curatedListSymbol,
+        address curator,
+        bytes memory lsp4MetadataURIOfLSP8_
+    ) external returns (address) {
+        CuratedListCollection curatedListAddress = new CuratedListCollection(
+            curatedListName,
+            curatedListSymbol,
+            curator,
+            lsp4MetadataURIOfLSP8_
+        );
+        return address(curatedListAddress);
+    }
+}
