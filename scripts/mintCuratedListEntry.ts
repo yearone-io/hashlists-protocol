f296af6927cae1be64cd2fa78d66a95ce79dbcfe
user.name <user@email>
2024-08-14 13:46:37 +0300

    only owner can mint from curated list as well as setting creator

--- scripts/mintCuratedListEntry.ts
+++ scripts/mintCuratedListEntry.ts
@@ -1,7 +1,7 @@
 import { ethers } from 'hardhat';
 
 const curatedListCollectionContract =
-  '0xA9E1F2999Dd7B429E564c64b4c5055FA62b2F8AA';
+  '0x0F46f73d49CB5d50ab7EE002dC751F3808e20529';
 
 async function main() {
 
@@ -12,7 +12,10 @@ async function main() {
 
   const addressOfEntryId = '0x1a2b3c4d5e6f708192a3b4c5d6e7f8091a2b3c4d5e6f708192a3b4c5d6e7f809';
   const tx = await curatedListcontract.mint(
+    "0x9F6bCab8d8d4403c4b78e96BB19b882E1E0fE947",
     addressOfEntryId,
+    true,
+    "0x",
     {
       gasLimit: 500000,
     }
